d84c8b439bd8abe36c904d35838e2b3d97b9adfb
ernie-hao <pdphiro@gmail.com>
2019-04-04 00:07:15 -0400

    Initial prototype`

--- src/App.js
+++ src/App.js
@@ -1,27 +1,132 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Controller, Scene } from 'react-scrollmagic';
+import { Tween, Timeline } from 'react-gsap';
 
+// import { map1 } from "./assets/map1.png"
+
+const TweenStyled = styled.div`
+  .section {
+    height: 100vh;
+    align-items: center;
+  }
+  .vis-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .tween {
+	  width: 100%;
+    height: 100%;
+    background-color: red;
+    margin: 0 !important;
+    position: relative;
+    right: calc(50% - 50px);
+  }
+  
+  .stagger {
+    width: 50px;
+    height: 50px;
+    left: 700px;
+    background-color: green;
+    position: relative;
+  }
+`;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <TweenStyled>
+        <div className="section" />
+        <div className="section" />
+        <div id="trigger" />
+        <div className="vis-container">
+          <Controller>
+            <Scene
+              triggerElement="#trigger"
+              duration={400}
+            >
+              {(progress) => (
+                <Tween
+                  to={{
+                    left: '0px',
+                    rotation: -360,
+                    width: '200px',
+                    height: '200px',
+                  }}
+                  ease="Strong.easeOut"
+                  totalProgress={progress}
+                  paused
+                >
+                  <img classname="tween" src="images/map2.png" alt="map1" />
+
+                </Tween>
+              )}
+            </Scene>
+            <Scene
+              triggerElement="#trigger"
+              duration={400}
+              pin={false}
+            >
+              {(progress) => (
+                <Tween
+                  from={{
+                    css: {
+                      left: '0px',
+                      rotation: -360,
+                      width: '200px',
+                      height: '200px',
+                    },
+                    ease: 'Strong.easeOut',
+                  }}
+                  to={{
+                    css: {
+                      left: '100px',
+                      rotation: -300,
+                      width: '50px',
+                      height: '50px',
+                    },
+                    ease: 'Strong.easeOut',
+                  }}
+                  totalProgress={progress}
+                  paused
+                >
+                  <img classname="tween" src="images/map1.png" alt="map1" />
+
+                </Tween>
+              )}
+            </Scene>
+            {/* <Scene
+            duration={500}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+            {(progress) => (
+              <div>
+                <Tween
+                  staggerFrom={{
+                    left: 700,
+                  }}
+                  staggerTo={{
+                    left: 0,
+                    ease: 'Back.easeOut',
+                  }}
+                  stagger={0.15}
+                  totalProgress={progress}
+                  paused
+                >
+                  <div className="stagger" />
+                  <div className="stagger" />
+                  <div className="stagger" />
+                  <div className="stagger" />
+                  <div className="stagger" />
+                </Tween>
+              </div>
+            )}
+          </Scene> */}
+          </Controller>
+        </div>
+        <div className="section" />
+        <div className="section" />
+      </TweenStyled>)
   }
 }
 
